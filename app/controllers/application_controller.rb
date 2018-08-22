7ac7c7ea1bedbe63ceeaa10262c332ea25eda065
Brittany Grebnov <brittanygrebnova@gmail.com>
2018-08-22 20:43:32 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,6 +12,7 @@ class App < Sinatra::Base
       @team_name = params[:team][:name]
       @team_motto = params[:team][:motto]
       @hero_name1 = params["team"]["members"][0]["name"]
+      @hero_power1 = params["team"]["members"][0]["power"]
       erb :team
     end
 
